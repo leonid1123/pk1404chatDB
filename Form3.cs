c68f756fbd4085145e707321ff1939e0b0c2f5f6
Leo <47742726+leonid1123@users.noreply.github.com>
2022-12-05 10:25:15 +0300

    Initial commit

---
+++ Form3.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace pk1404chatDB
+{
+    public partial class Form3 : Form
+    {
+        public Form3()
+        {
+            InitializeComponent();
+        }
+    }
+}
